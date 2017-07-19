9d4dbada043c43cc2aef7852d9b1b6569c04bbf4
Justyna G <justykag@wp.pl>
2017-07-19 15:09:45 +0200

    karuzela poprawa

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 $(function() { 
 
 	var carouselList = $("#carousel ul");
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
+	var pause = 3000;
 	
-
 	setInterval(function() {
-		carouselList.animate({'marginLeft': '-400px'}, 2000, function() {
+		carouselList.animate({'marginLeft': '-400px'}, 1000, function() {
+
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
 
 			lastItem.after(firstItem); 
 			carouselList.css({marginLeft:0});
 		});
-	});
+	}, pause);
 });
 
 
